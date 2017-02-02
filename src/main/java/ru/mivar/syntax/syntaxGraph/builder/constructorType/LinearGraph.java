2ceed3ca936aedee4604ca57f9b15ff7a05c817b
s.kharchenko <s.kharchenko@mivar.ru>
2017-02-03 01:03:25 +0300

    добавил карту SentenceWord -> Vertex

--- src/main/java/ru/mivar/syntax/syntaxGraph/builder/constructorType/LinearGraph.java
+++ src/main/java/ru/mivar/syntax/syntaxGraph/builder/constructorType/LinearGraph.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 public class LinearGraph implements IConstructorGraph {
 
-    private static final Logger LOGGER = LogManager.getLogger(Builder.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(LinearGraph.class.getName());
 
     private final Connection connection;
 
--- src/main/java/ru/mivar/syntax/syntaxGraph/components/SyntaxGraph.java
+++ src/main/java/ru/mivar/syntax/syntaxGraph/components/SyntaxGraph.java
@@ -1,11 +1,15 @@
 package ru.mivar.syntax.syntaxGraph.components;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SyntaxGraph {
+
     private Set<Vertex> vertices = new HashSet<>();
     private Set<Edge> edges = new HashSet<>();
+    private Map<SentenceWord, Set<Vertex>> wordVerticesMap = new HashMap<>();
+
+    public SyntaxGraph() {
+    }
 
     public Set<Vertex> getVertices() {
         return vertices;
@@ -16,10 +20,18 @@ public class SyntaxGraph {
     }
 
     void addVertex(Vertex vertex) {
-        vertices.add(vertex);
+        SentenceWord sentenceWord = vertex.getWord();
+        if (!wordVerticesMap.containsKey(sentenceWord)) {
+            wordVerticesMap.put(sentenceWord, new HashSet<>());
+        }
+        wordVerticesMap.get(sentenceWord).add(vertex);
     }
 
-    void addEdge(Edge edge){
+    void addEdge(Edge edge) {
         edges.add(edge);
+    }
+
+    public Map<SentenceWord, Set<Vertex>> getWordVerticesMap() {
+        return wordVerticesMap;
     }
 }
